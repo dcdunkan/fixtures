d48cdf925cd6d5313b503d77d35385c1294464c5
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2025-08-07 14:00:58 +0530

    chore: setup dprint

--- react-router.config.js
+++ react-router.config.js
@@ -1,5 +1,5 @@
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+    // Config options...
+    // Server-side render by default, to enable SPA mode set this to `false`
+    ssr: true,
 };
