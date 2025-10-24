d0effd2a10a877553aaf97cde7c13949a1b35a8b
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2025-10-24 10:37:51 +0530

    feat: add home screen stuff

--- src/routes/clubs/[clubId]/TournamentsSection.jsx
+++ src/routes/clubs/[clubId]/TournamentsSection.jsx
@@ -4,16 +4,19 @@ import CreateTournamentDialog from "./CreateTournamentDialog";
 import TournamentsList from "./TournamentList";
 
 export default function TournamentsSection() {
-    const { tournaments } = useContext(ClubContext);
+    const { tournaments, club } = useContext(ClubContext);
 
     return (
         <div className="space-y-4 w-full">
             <div className="flex justify-between place-items-center gap-4">
                 <h3 className="font-medium text-xl">Tournaments</h3>
 
-                <div>
-                    <CreateTournamentDialog />
-                </div>
+                {club.data.membership.role !== "member"
+                    && (
+                        <div>
+                            <CreateTournamentDialog />
+                        </div>
+                    )}
             </div>
 
             <TournamentsList />
--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,19 +1,64 @@
 import { useAuth } from "@/hooks/auth";
+import { CircleXIcon } from "lucide-react";
+import { LoaderCircleIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import OngoingTournamentsList from "./OngoingTournamentsList";
 
 export default function HomePage() {
     const { data, api } = useAuth();
+    const [clubs, setClubs] = useState({
+        state: "pending",
+        message: "Loading clubs...",
+        data: [],
+    });
+
+    useEffect(() => {
+        api
+            .get("user/tournaments")
+            .json()
+            .then((clubsData) => {
+                console.log(clubsData);
+                setClubs({
+                    state: "resolved",
+                    data: clubsData,
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+                setClubs({
+                    state: "rejected",
+                    message: "Unable to retrieve clubs",
+                });
+            });
+    }, [api]);
 
     return (
         <div className="space-y-4">
             <title>Fixtures</title>
-
-            {/* <h2 className="font-black text-4xl">What's Happening?</h2> */}
-
-            {/* <h2 className="font-semibold text-lg">Ongoing Matches</h2> */}
-
-            <h2 className="font-semibold text-lg">Ongoing Tournaments</h2>
-
-            <div>You are not part of any clubs. Create one to get started!</div>
+            <h2 className="font-semibold text-4xl">Welcome, {data.name}!</h2>
+            <div>
+                {clubs.state === "pending"
+                    ? (
+                        <div className="text-muted-foreground inline-flex gap-2 place-items-center">
+                            <LoaderCircleIcon className="animate-spin size-4" /> {clubs.message}
+                        </div>
+                    )
+                    : clubs.state === "resolved"
+                    ? (
+                        clubs.data.length === 0
+                            ? (
+                                <div className="text-muted-foreground">
+                                    You are not part of any club yet. Create one to get started!
+                                </div>
+                            )
+                            : <OngoingTournamentsList tournaments={clubs.data} />
+                    )
+                    : (
+                        <div className="text-destructive inline-flex gap-2 place-items-center">
+                            <CircleXIcon className="size-4" /> {clubs.message}
+                        </div>
+                    )}
+            </div>
         </div>
     );
 }
